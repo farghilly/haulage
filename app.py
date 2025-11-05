fbd8067ecab493280c865ac9fff3d7bae8828bc7
farghilly <mohamed.mohamed39@cchellenic.com>
2025-11-05 08:51:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -312,7 +312,7 @@ with tab_fleet:
         fig1 = px.line(df_dead_head_distance, x='actual_shipment_start', y='dead_head_distance',
                        title='Dead Head Distance Over Time', markers=True, template='plotly_white')
         total_deadhead = round(filtered_df['dead_head_distance'].sum())
-        st.metric(label="Total Dead Head Distance", value= f"{total_deadhead:,.1f}M")
+        st.metric(label="Total Dead Head Distance", value= f"{total_deadhead:,.0f}")
         st.plotly_chart(fig1, use_container_width=True)
     with col2:
         st.subheader("📈 Total Distance Travelled Over Time")
@@ -320,7 +320,7 @@ with tab_fleet:
         fig2 = px.line(df_distance_travelled, x='actual_shipment_start', y='total_distance',
                        title='Total Distance Travelled Over Time', markers=True, template='plotly_white')
         total_distance = round(filtered_df['total_distance'].sum())
-        st.metric(label="Total Distance Travelled", value= f"{total_distance:,.1f}M,")
+        st.metric(label="Total Distance Travelled", value= f"{total_distance:,.0f}")
         st.plotly_chart(fig2, use_container_width=True)
 
     st.markdown("---")
